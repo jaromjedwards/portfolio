381cda7217caed7c123e4beba2db63a6cd6bdf03
Jarom Edwards <jaromjedwards@gmail.com>
2023-10-14 13:18:37 -0700

    timer added

--- games/scripts/speed-test.js
+++ games/scripts/speed-test.js
@@ -1,7 +1,7 @@
 // Declare variables and initialize DOM elements
 const userInput = document.getElementById('userInput');
 const sentenceDisplay = document.getElementById('sentence');
-const scoreDisplay = document.getElementById('score');
+const scoreDisplay = document.getElementById('score.js');
 
 // Define sentences
 const sentences = [
@@ -17,10 +17,21 @@ const sentences = [
     "The early bird catches the worm"
 ];
 
+//start a timer and put it in an element
+function add1() {
+    seconds += 1; // Add 1 to the number
+    console.log(seconds); // Print the updated number to the console
+    scoreDisplay.textContent = `${seconds} Seconds`; // Update the display
+}
+
 // Initialize the game when the start button is clicked
 function startGame() {
-    scoreDisplay.textContent = 'Score'
+    seconds = 0;
+    add1();
+    addByOne = setInterval(add1, 1000);
+    scoreDisplay.textContent = `${seconds} Second`;
     userInput.value = '';
+    userInput.focus();
     const startTime = getTime();
     const sentence = getSentence();
 
@@ -28,6 +39,7 @@ function startGame() {
         if (event.key === 'Enter') {
             const endTime = getTime();
             const { speed, difference, score } = updateScore(sentence, userInput.value, startTime, endTime);
+            clearInterval(addByOne);
             displayScore(speed, difference, score);
         }
     });
@@ -54,7 +66,7 @@ function updateScore(sentence, userInput, startTime, endTime) {
 
 // Display the score
 function displayScore(speed, difference, score) {
-    scoreDisplay.textContent = `Your time was ${speed.toFixed(2)}s, and you had ${difference} mistakes. Your score is ${score.toFixed(2)}`;
+    scoreDisplay.innerHTML = `Speed: ${speed.toFixed(2)} seconds!<br>Mistakes: ${difference}<br>Score: ${score.toFixed(2)}`;
 }
 
 // Get the current time in milliseconds
